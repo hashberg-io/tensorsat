c39c9c874af997c39925aa5042355caf37d6cae0
Ayberk Tosun <ayberk.tosun@gmail.com>
2025-01-20 17:13:19 +0000

    Finish and start cleaning up

--- notebooks/prototypes/2025-01-09-cotengra-prototype/_formula.py
+++ notebooks/prototypes/2025-01-09-cotengra-prototype/_formula.py
@@ -1,5 +1,6 @@
 """
-Representation of propositional logical formulae along with machinery for CNF transformation.
+Representation of propositional formulae along with basic machinery for CNF
+transformation.
 """
 
 from enum import Enum
@@ -7,17 +8,17 @@ from typing import (List, Optional)
 
 
 class Operation(Enum):
-    VAR    = 1  ## variable
-    NEG    = 2  ## negation
-    CONJ   = 3  ## conjunction
-    DISJ   = 4  ## disjunction
-    IMPL   = 5  ## implication
-    BIIMPL = 6  ## bi-implication
-    TRUTH  = 7  ## top
-    FALSUM = 8  ## bottom
+    VAR = 1     # variable
+    NEG = 2     # negation
+    CONJ = 3    # conjunction
+    DISJ = 4    # disjunction
+    IMPL = 5    # implication
+    BIIMPL = 6  # bi-implication
+    TRUTH = 7   # top
+    FALSUM = 8  # bottom
 
 
-def arity(op : Operation) -> int:
+def arity(op: Operation) -> int:
     """The arity of a given operation."""
     if op == Operation.VAR:
         return 0
@@ -35,101 +36,107 @@ def arity(op : Operation) -> int:
 
 class Formula:
 
-    def __init__(self, sort) -> None:
-         self.sort     : Operation  = sort
-         self.name     : str | None = None
-         self.subtrees : List[Formula | None] = []
+    def __init__(self, op) -> None:
+        self.operation: Operation = op
+        self.name: str | None = None
+        self.subtrees: List[Formula | None] = []
 
-         if arity(sort) == 0:
-             if sort == Operation.VAR:
-                self.name     = None
+        if arity(op) == 0:
+            if op == Operation.VAR:
+                self.name = None
                 self.subtrees = []
-             elif sort == Operation.TRUTH or sort == Operation.FALSUM:
-                self.name     = None
+            elif op == Operation.TRUTH or op == Operation.FALSUM:
+                self.name = None
                 self.subtrees = []
-         elif arity(sort) == 1:
-             self.subtrees = [None]
-         elif arity(sort) == 2:
-             self.subtrees = [None, None]
+        elif arity(op) == 1:
+            self.subtrees = [None]
+        elif arity(op) == 2:
+            self.subtrees = [None, None]
 
-    def set_subformula_of_unary_operation(self, e):
-        if arity(self.sort) == 1:
-            self.subtrees[0] = e
+    @classmethod
+    def set_subformula_of_unary_operation(self, phi):
+        if arity(self.operation) == 1:
+            self.subtrees[0] = phi
         else:
             raise ValueError("Arity mismatch")
 
+    @classmethod
     def set_subformulae_of_binary_operation(self, phi, psi):
-        if arity(self.sort) == 2:
+        if arity(self.operation) == 2:
             self.subtrees[0] = phi
             self.subtrees[1] = psi
         else:
             raise ValueError("Arity mismatch")
 
+    @classmethod
     def subformula_of_unary_operation(self):
-        if arity(self.sort) == 1:
+        if arity(self.operation) == 1:
             return self.subtrees[0]
         else:
             raise ValueError("Arity mismatch")
 
+    @classmethod
     def left_subformula_of_binary_operation(self):
-        if arity(self.sort) == 2:
+        if arity(self.operation) == 2:
             return self.subtrees[0]
         else:
             raise ValueError("Arity mismatch")
 
+    @classmethod
     def right_subformula_of_binary_operation(self):
-        if arity(self.sort) == 2:
+        if arity(self.operation) == 2:
             return self.subtrees[1]
         else:
             raise ValueError("Arity mismatch")
 
 
-def variable(s : str) -> Formula:
-    """Construct a formula consisting only of a variable identified with the name `s`."""
-    phi      = Formula(Operation.VAR)
+def variable(s: str) -> Formula:
+    """Construct a variable with name `s`."""
+    phi = Formula(Operation.VAR)
     phi.name = s
     return phi
 
 
-def binary_expression(op : Operation, phi : Formula, psi : Formula) -> Formula:
-    """Construct a binary formula consisting of the application of `op` to `phi` and `psi`."""
+def binary_formula(op: Operation, phi: Formula, psi: Formula) -> Formula:
+    """Construct a formula with a binary operation."""
     theta = Formula(op)
     theta.set_subformulae_of_binary_operation(phi, psi)
     return theta
 
 
-def unary_formula(op : Operation, phi : Formula) -> Formula:
+def unary_formula(op: Operation, phi: Formula) -> Formula:
+    """Construct a formula with a unary operation."""
     psi = Formula(op)
     psi.set_subformula_of_unary_operation(phi)
     return psi
 
 
-def neg(phi : Formula) -> Formula:
+def neg(phi: Formula) -> Formula:
     """Construct the negation of a formula `phi`."""
     return unary_formula(Operation.NEG, phi)
 
 
-def conj(phi : Formula, psi : Formula) -> Formula:
-    """Shorthand for the conjunction of two formulae `phi` and `psi`."""
-    return binary_expression(Operation.CONJ, phi, psi)
+def conj(phi: Formula, psi: Formula) -> Formula:
+    """Conjunction of two formulae `phi` and `psi`."""
+    return binary_formula(Operation.CONJ, phi, psi)
 
 
-def disj(e1 : Formula, e2 : Formula) -> Formula:
-    """Shorthand for constructing a disjunction."""
-    return binary_expression(Operation.DISJ, e1, e2)
+def disj(phi: Formula, psi: Formula) -> Formula:
+    """Disjunction of two formulae `phi` and `psi`."""
+    return binary_formula(Operation.DISJ, phi, psi)
 
 
-falsum : Formula = Formula(Operation.FALSUM)
-
-def impl(phi : Formula, psi : Formula) -> Formula:
-    return binary_expression(Operation.IMPL, phi, psi)
 
 
-def biimpl(phi : Formula, psi : Formula) -> Formula:
-    return binary_expression(Operation.BIIMPL, phi, psi)
+def impl(phi: Formula, psi: Formula) -> Formula:
+    return binary_formula(Operation.IMPL, phi, psi)
 
 
-def show_operation(op : Operation) -> str:
+def biimpl(phi: Formula, psi: Formula) -> Formula:
+    return binary_formula(Operation.BIIMPL, phi, psi)
+
+
+def show_operation(op: Operation) -> str:
     if op == Operation.VAR:
         return "Var"
     elif op == Operation.NEG:
@@ -148,46 +155,46 @@ def show_operation(op : Operation) -> str:
         return "Disj"
 
 
-def show_formula(phi : Formula) -> str:
-    if arity(phi.sort) == 0:
+def show_formula(phi: Formula) -> str:
+    if arity(phi.operation) == 0:
         return f"(Var \"{phi.name}\")"
-    elif arity(phi.sort) == 1:
-        s1 : str = show_operation(phi.sort)
-        s2 : str = show_formula(phi.subformula_of_unary_operation())
+    elif arity(phi.operation) == 1:
+        s1: str = show_operation(phi.operation)
+        s2: str = show_formula(phi.subformula_of_unary_operation())
         return f"({s1} {s2})"
     else:
-        assert(arity(phi.sort) == 2)
-        sa : str = show_operation(phi.sort)
-        sb : str = show_formula(phi.left_subformula_of_binary_operation())
-        sc : str = show_formula(phi.right_subformula_of_binary_operation())
+        assert arity(phi.operation) == 2
+        sa: str = show_operation(phi.operation)
+        sb: str = show_formula(phi.left_subformula_of_binary_operation())
+        sc: str = show_formula(phi.right_subformula_of_binary_operation())
         return f"({sa} {sb} {sc})"
 
 
-def eliminate_implications(phi : Formula) -> Formula:
-    """Eliminate implications and bi-implications from a given formula `phi` by repeatedly applying
-       the identity `p => q <-> ¬p ∨ q`."""
-    if arity(phi.sort) == 0:
+def eliminate_implications(phi: Formula) -> Formula:
+    """Eliminate implications and bi-implications from a given formula."""
+    if arity(phi.operation) == 0:
         return phi
-    elif arity(phi.sort) == 1:
-        if phi.sort == Operation.NEG:
-            return neg(eliminate_implications(phi.subformula_of_unary_operation()))
+    elif arity(phi.operation) == 1:
+        if phi.operation == Operation.NEG:
+            phi0 = phi.subformula_of_unary_operation()
+            return neg(eliminate_implications(phi0))
         else:
             raise ValueError("Unknown logical operation")
     else:
-        assert(arity(phi.sort) == 2)
-        psi    = phi.left_subformula_of_binary_operation()
-        theta  = phi.right_subformula_of_binary_operation()
-        psi0   = eliminate_implications(psi)
+        assert arity(phi.operation) == 2
+        psi = phi.left_subformula_of_binary_operation()
+        theta = phi.right_subformula_of_binary_operation()
+        psi0 = eliminate_implications(psi)
         theta0 = eliminate_implications(theta)
-        if phi.sort == Operation.IMPL:
+        if phi.operation == Operation.IMPL:
             return disj(neg(psi0), theta0)
-        elif phi.sort == Operation.BIIMPL:
+        elif phi.operation == Operation.BIIMPL:
             return eliminate_implications(conj(psi0, theta0))
         else:
-            return binary_expression(phi.sort, psi0, theta0)
+            return binary_formula(phi.operation, psi0, theta0)
 
 
-def dualize(op : Operation) -> Optional[Operation]:
+def dualize(op: Operation) -> Optional[Operation]:
     if op == Operation.CONJ:
         return Operation.DISJ
     elif op == Operation.DISJ:
@@ -195,84 +202,127 @@ def dualize(op : Operation) -> Optional[Operation]:
     else:
         return None
 
-def demorganize(phi : Formula) -> Formula:
-    """Transforms a formula by pushing all negations inside through the repeated application of the de Morgan laws.
-       N.B. Should not be used on formulae containing implications."""
 
-    if arity(phi.sort) == 0:
+def demorganize(phi: Formula) -> Formula:
+    """Applies the de Morgan laws repeatedly and removes double negations.
+    """
+
+    if arity(phi.operation) == 0:
         return phi
-    elif arity(phi.sort) == 1:
-        ## Currently, negation is the only unary operation so the check below is redundant. But this might change
-        ## in the future.
+    elif arity(phi.operation) == 1:
+        # Currently, negation is the only unary operation so the check below is
+        # redundant. But this might change in the future so I'm keeping this
+        # here.
         psi: Formula = phi.subformula_of_unary_operation()
-        if phi.sort == Operation.NEG:
-            if psi.sort == Operation.NEG:
-                ## This case means we have just seen a double-negation, which we get rid of.
+        if phi.operation == Operation.NEG:
+            if psi.operation == Operation.NEG:
+                # This case means we have just seen a double-negation, which we
+                # get rid of.
                 return demorganize(psi.subformula_of_unary_operation())
-            elif psi.sort in [Operation.CONJ, Operation.DISJ]:
-                ## This case entails having seen a formula of the form `Neg(BinOp(psi, theta))`.
-                theta = phi.subformula_of_unary_operation().left_subformula_of_binary_operation()
-                gamma = phi.subformula_of_unary_operation().right_subformula_of_binary_operation()
-                op    = dualize(psi.sort)
-                assert(op is not None)
-                return binary_expression(op, demorganize(neg(theta)), demorganize(neg(gamma)))
+            elif psi.operation in [Operation.CONJ, Operation.DISJ]:
+                # This case means we have just seen a formula of the
+                # form `Neg(BinOp(psi, theta))`.
+                theta = psi.left_subformula_of_binary_operation()
+                gamma = psi.right_subformula_of_binary_operation()
+                op: Operation = dualize(psi.operation)
+                assert op is not None
+                theta0 = demorganize(neg(theta))
+                gamma0 = demorganize(neg(gamma))
+                return binary_formula(op, theta0, gamma0)
             else:
-                ## Reaching this case means having seen a formula of the form `Neg(phi)`, where phi is neither
-                ## of a conjunction, a disjunction, or a double negation. In this case, we simply do a recursive
-                ## call.
+                # This case means we have just seen a formula of the form
+                # `Neg(phi)`, where `phi` is neither of: a conjunction, a
+                # disjunction, or a double negation. In this case, we simply do
+                # a recursive call.
                 return neg(demorganize(psi))
         else:
             raise ValueError("Unknown logical operation")
     else:
-        assert(arity(phi.sort) == 2)
-        if phi.sort == Operation.IMPL or phi.sort == Operation.BIIMPL:
-            raise ValueError("The `demorganize` function should not be used on formulae containing implications or bi-implications.")
-        elif phi.sort == Operation.CONJ or phi.sort == Operation.DISJ:
-            ## redundant check in case other operations are added in the future
-            psi   : Formula = phi.left_subformula_of_binary_operation()
-            theta : Formula = phi.right_subformula_of_binary_operation()
-            return binary_expression(phi.sort, demorganize(psi), demorganize(theta))
+        assert arity(phi.operation) == 2
+        op = phi.operation
+        if op == Operation.IMPL or op == Operation.BIIMPL:
+            raise ValueError("Formula contains implication.")
+        elif op == Operation.CONJ or phi.operation == Operation.DISJ:
+            # Redundant check in case other operations are added in the future.
+            psi: Formula = phi.left_subformula_of_binary_operation()
+            theta: Formula = phi.right_subformula_of_binary_operation()
+            psi0 = demorganize(psi)
+            theta0 = demorganize(theta)
+            return binary_formula(phi.operation, psi0, theta0)
         else:
             raise ValueError("Unknown logical operation")
 
 
-def merge(cnf1 : Formula, cnf2 : Formula) -> Formula:
-    """Construct disjunctions of formulae in a way that preserves the CNF-property (for formulae that are already in CNF)."""
-    if cnf1.sort == Operation.CONJ:
+def merge(cnf1: Formula, cnf2: Formula) -> Formula:
+    """Construct disjunctions of formulae in a way that preserves the
+       CNF property (for formulae that are already in CNF).
+    """
+    if cnf1.operation == Operation.CONJ:
         cnf1a = cnf1.left_subformula_of_binary_operation()
         cnf1b = cnf1.right_subformula_of_binary_operation()
         return conj(merge(cnf1a, cnf2), merge(cnf1b, cnf2))
-    elif cnf2.sort == Operation.CONJ:
+    elif cnf2.operation == Operation.CONJ:
         cnf2a = cnf2.left_subformula_of_binary_operation()
         cnf2b = cnf2.right_subformula_of_binary_operation()
         return conj(merge(cnf1, cnf2a), merge(cnf1, cnf2b))
     else:
         return disj(cnf1, cnf2)
 
-def distribute(phi : Formula) -> Formula:
-    """Pushes all disjunctions inside by repeatedly applying the distributivity law."""
-    if arity(phi.sort) == 0:
+
+def distribute(phi: Formula) -> Formula:
+    """Pushes all disjunctions inside."""
+    op = phi.operation
+    if arity(op) == 0:
         return phi
-    elif arity(phi.sort) == 1:
-        return unary_formula(phi.sort, phi.subformula_of_unary_operation())
-    elif arity(phi.sort) == 2:
-        if phi.sort == Operation.CONJ:
-            psi   = phi.left_subformula_of_binary_operation()
+    elif arity(op) == 1:
+        return unary_formula(op, phi.subformula_of_unary_operation())
+    elif arity(op) == 2:
+        if op == Operation.CONJ:
+            psi = phi.left_subformula_of_binary_operation()
             theta = phi.right_subformula_of_binary_operation()
             return conj(distribute(psi), distribute(theta))
-        elif phi.sort == Operation.DISJ:
-            psi   = phi.left_subformula_of_binary_operation()
+        elif op == Operation.DISJ:
+            psi = phi.left_subformula_of_binary_operation()
             theta = phi.right_subformula_of_binary_operation()
             return merge(distribute(psi), distribute(theta))
         else:
             raise ValueError("Unexpected logical operation in `distribute`.")
 
 
-def to_cnf(phi : Formula) -> Formula:
+def to_cnf(phi: Formula) -> Formula:
+    """Convert a formula into CNF form."""
     return distribute(demorganize(eliminate_implications(phi)))
 
-lem       : Formula = disj(variable("p"), neg(variable("p")))
-meet_conj : Formula = impl(conj(variable("p"), variable("q")), conj(variable("q"), variable("p")))
+
+def is_negated_variable(phi: Formula) -> bool:
+    if phi.operation == Operation.NEG:
+        psi = phi.subformula_of_unary_operation()
+        return psi.operation == Operation.VAR
+    else:
+        return False
+
+
+def literal_list(phi: Formula) -> list[tuple[bool, str]]:
+    """Takes a formula consisting of disjunctions in literals and returns a
+       representation of it is a clause."""
+    if phi.operation == Operation.VAR:
+        return [(True, phi.name)]
+    elif is_negated_variable(phi):
+        return [(False, phi.subformula_of_unary_operation().name)]
+    else:
+        assert phi.operation == Operation.DISJ
+        psi = phi.left_subformula_of_binary_operation()
+        theta = phi.right_subformula_of_binary_operation()
+        return literal_list(psi) + literal_list(theta)
+
+
+falsum: Formula = Formula(Operation.FALSUM)
+
+
+var_p = variable("p")
+var_q = variable("q")
+lem: Formula = disj(variable("p"), neg(variable("p")))
+meet_conj: Formula = impl(conj(var_p, var_q), conj(var_q, var_p))
 
 print(show_formula(eliminate_implications(meet_conj)))
 print(show_formula(demorganize(eliminate_implications(meet_conj))))
