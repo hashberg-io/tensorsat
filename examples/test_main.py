5d1cc4d281a589d9a4e066bbf416cb38b29ec5af
Ayberk Tosun <ayberk.tosun@gmail.com>
2025-02-16 15:16:23 +0000

    refactor: clean up the tests

--- examples/test_main.py
+++ examples/test_main.py
@@ -1,25 +1,24 @@
 from tensorsat.lib.sat import *
 
 def test_dimacs_parser_unit1():
-    dimacs_unit1 = ""
-    dimacs_unit2 = ""
-    with open("examples/kissat/cnf/unit1.cnf") as file_unit1:
-        dimacs_unit1 = file_unit1.read()
+    dimacs = ""
+    with open("examples/kissat/cnf/unit1.cnf") as f:
+        dimacs = f.read()
 
-    cnf_unit1: CNFInstance = CNFInstance.from_dimacs(dimacs_unit1)
-    expected_unit1: list[Clause] = [(1,), (-1,)]
+    cnf: CNFInstance = CNFInstance.from_dimacs(dimacs)
+    expected: tuple[Clause] = ((1,), (-1,))
 
-    assert set(cnf_unit1.clauses) == set(expected_unit1)
+    assert cnf.clauses == expected
 
 def test_dimacs_parser_unit2():
-    dimacs_unit2 = ""
-    with open("examples/kissat/cnf/unit2.cnf") as file_unit2:
-        dimacs_unit2 = file_unit2.read()
+    dimacs = ""
+    with open("examples/kissat/cnf/unit2.cnf") as f:
+        dimacs = f.read()
 
-    cnf_unit2: CNFInstance = CNFInstance.from_dimacs(dimacs_unit2)
-    expected_unit2: list[Clause] = [(1,), (-1,)]
+    cnf: CNFInstance = CNFInstance.from_dimacs(dimacs)
+    expected: tuple[Clause] = ((-1,), (1,))
 
-    assert set(cnf_unit2.clauses) == set(expected_unit2)
+    assert cnf.clauses == expected
 
 def test_dimacs_parser_unit3():
     dimacs = ""
@@ -27,9 +26,9 @@ def test_dimacs_parser_unit3():
         dimacs = f.read()
 
     cnf: CNFInstance = CNFInstance.from_dimacs(dimacs)
-    expected: list[Clause] = [(2,), (-2,)]
+    expected: tuple[Clause] = ((2,), (-2,))
 
-    assert set(cnf.clauses) == set(expected)
+    assert cnf.clauses == expected
 
 def test_dimacs_parser_unit4():
     dimacs = ""
@@ -37,12 +36,12 @@ def test_dimacs_parser_unit4():
         dimacs = f.read()
 
     cnf: CNFInstance = CNFInstance.from_dimacs(dimacs)
-    expected: list[Clause] = [(7,),
-                              (-7, -1),
-                              (-7, 1, 3),(-7, 1, 5),
-                              (-7, -3, -5)]
+    expected: tuple[Clause] = ((7,),
+                               (-7, -1),
+                               (-7, 1, 3),(-7, 1, 5),
+                               (-7, -3, -5))
 
-    assert set(cnf.clauses) == set(expected)
+    assert cnf.clauses == expected
 
 def test_dimacs_parser_unit5():
     dimacs = ""
