54a95ac8ddecece74c7f71a7dc3551cce2008cdb
Ayberk Tosun <ayberk.tosun@gmail.com>
2025-04-06 16:51:31 +0100

    test: set up the new formulae benchmarks

--- tensorsat/benchmark.py
+++ tensorsat/benchmark.py
@@ -1,63 +1,38 @@
-from tensorsat.lib.bincirc import and_
+# from tensorsat.lib.bincirc import and_
 from tensorsat.lib.sat import *
 from tensorsat.lang.fin_rel import FinRel
 from tensorsat.contractions.cotengra import CotengraContraction
+from tensorsat.lib.formula import *
 import numpy as np
 import lzma
 import sys
+from z3 import Solver, sat
 
+def benchmark_formulae() -> None:
+    solver = Solver()
+    with open("examples/formulae/formulae.txt", "r") as f:
+        num_sat = 0
+        num_unsat = 0
+        for line in f:
+            if len(line) > 1:
+                phi: Formula | None = eval(line)
+                if phi:
+                    solver.push()
+                    solver.add(to_z3_formula(phi))
+
+                    if solver.check() == sat:
+                        # print("sat")
+                        num_sat += 1
+                    else:
+                        # print("unsat")
+                        # print(line)
+                        num_unsat += 1
+
+                    solver.pop()
+                else:
+                    raise ParseError("Could not read formula.")
+
+        print(f"{num_sat} sat, {num_unsat} unsatisfiable.")
 
 def main() -> None:
-    if len(sys.argv) <= 1:
-        print("Please provide a file name as input.")
-        sys.exit(1)
-
-    filename: str = sys.argv[1]
-
-    print(f"Filename is {filename}")
-
-    dimacs: str = ""
-
-    if filename.endswith(".xz"):
-        with lzma.open(filename, mode="rt") as f:
-            dimacs = f.read()
-    else:
-        with open(filename, mode="r") as f:
-            dimacs = f.read()
-
-    cnf: CNFInstance = CNFInstance.from_dimacs(dimacs)
-
-    cnf_diagram = cnf.diagram()
-    cnf_sat_diagram = (cnf.inputs(None)>>cnf_diagram).flatten()
-    cnf_sat_contraction = CotengraContraction(FinRel, cnf_sat_diagram.wiring)
-
-    result: bool = cnf_sat_contraction.contract(cnf_sat_diagram, progbar=False, autojit=False)
-
-    if result:
-        print("sat")
-        sys.exit(10)
-    else:
-        print("unsat")
-        sys.exit(20)
-
-    # print(cnf_solutions)
-
-    # print(cnf)
-
-    # print("This is tensorsat")
-    # print("-----------------\n")
-
-    # print("Representation of logical conjunction as a tensor.")
-    # print(and_.tensor)
-    # print("\n")
-
-    # and_tensor = and_.tensor
-
-    # print({
-    #     point
-    #     for point in np.ndindex(and_.tensor.shape)
-    #     if and_.tensor[point]
-    # })
-
-    # print("\n")
-    # print(and_tensor.reshape(4, 2))
+    benchmark_formulae()
