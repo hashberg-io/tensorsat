13b8f3d57984ff1a33ce364248b4eb8cd1b365fa
Ayberk Tosun <ayberk.tosun@gmail.com>
2025-04-06 17:04:13 +0100

    test: print out the time

--- tensorsat/benchmark.py
+++ tensorsat/benchmark.py
@@ -6,6 +6,7 @@ from tensorsat.lib.formula import *
 import numpy as np
 import lzma
 import sys
+import time
 from z3 import Solver, sat
 
 def benchmark_formulae() -> None:
@@ -13,13 +14,16 @@ def benchmark_formulae() -> None:
     with open("examples/formulae/formulae.txt", "r") as f:
         num_sat = 0
         num_unsat = 0
+        time_total = 0
         for line in f:
             if len(line) > 1:
                 phi: Formula | None = eval(line)
                 if phi:
                     solver.push()
-                    solver.add(to_z3_formula(phi))
+                    f = to_z3_formula(phi)
 
+                    start_time = time.time()
+                    solver.add(f)
                     if solver.check() == sat:
                         # print("sat")
                         num_sat += 1
@@ -29,10 +33,14 @@ def benchmark_formulae() -> None:
                         num_unsat += 1
 
                     solver.pop()
+                    time_taken = (time.time() - start_time) * 1000
+                    time_total += time_taken
+                    print("{} ms".format(time_taken))
                 else:
                     raise ParseError("Could not read formula.")
 
         print(f"{num_sat} sat, {num_unsat} unsatisfiable.")
+        print("Total time: {} seconds".format(time_total / 1000))
 
 def main() -> None:
     benchmark_formulae()
