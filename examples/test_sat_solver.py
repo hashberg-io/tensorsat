79e7d98422e95cc1d6f0fcee25c1aefe44f807a9
Ayberk Tosun <ayberk.tosun@gmail.com>
2025-04-06 17:46:49 +0100

    test: add the `diamond2` and `diamond3` examples

--- examples/test_sat_solver.py
+++ examples/test_sat_solver.py
@@ -160,6 +160,13 @@ def test_example_diamond1() -> None:
     is_sat: bool = check_sat_file("diamond1")
     assert(not is_sat)
 
+def test_example_diamond2() -> None:
+    is_sat: bool = check_sat_file("diamond2")
+    assert(not is_sat)
+
+def test_example_diamond3() -> None:
+    is_sat: bool = check_sat_file("diamond3")
+    assert(not is_sat)
 
 ## TODO: figure out why the following fails.
 # def test_example_unit3() -> None:
