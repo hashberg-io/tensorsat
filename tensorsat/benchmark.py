935558974de1ef00f92b7d16e51c22af72c224bd
Ayberk Tosun <ayberk.tosun@gmail.com>
2025-04-17 14:53:10 +0100

    test: continue the experiments

--- tensorsat/benchmark.py
+++ tensorsat/benchmark.py
@@ -9,14 +9,16 @@ import numpy as np
 import lzma
 import sys
 import time
+import cotengra as ctg
 from z3 import Solver, sat
 
 def benchmark_formulae() -> None:
     solver = Solver()
-    with open("examples/counterexample3.txt", "r") as f:
+    with open("examples/all_shuffled.txt", "r") as f:
         num_sat = 0
         num_unsat = 0
-        time_total = 0
+        time_total_z3 = 0
+        time_total_tensorsat = 0
         num_errors = 0
         i = 0
         for line in f:
@@ -31,46 +33,51 @@ def benchmark_formulae() -> None:
                     # print("Constructing diagram...")
                     diag = diagram_of_formula(phi)
                     d = diag.diagram()
-                    draw_diagram(
-                        d,
-                        node_label={ "box": binop_labels, "wire": str },
-                        layout="circuit",
-                        simplify_wires=False,
-                        figsize=(8, 4)
-                    )
+                    # draw_diagram(
+                    #     d,
+                    #     node_label={ "box": binop_labels, "wire": str },
+                    #     layout="circuit",
+                    #     simplify_wires=False,
+                    #     figsize=(8, 4)
+                    # )
                     d_sat = d.flatten()
                     # print("Done.")
-                    cnf_sat_contraction = CotengraContraction(FinRel, d.wiring)
-                    result = bool(cnf_sat_contraction.contract(d))
-                    print("TensorSat: {}".format(result))
+                    rgo = ctg.pathfinders.path_basic.RandomGreedyOptimizer(max_repeats=1024, costmod=(0.1, 2.0), temperature=(0.1, 0.3), simplify=False, parallel=True, accel=True)
+                    cnf_sat_contraction = CotengraContraction(FinRel, d_sat.wiring, optimize=rgo)
 
-                    start_time = time.time()
+                    start_time_tensorsat = time.time()
+                    result = bool(cnf_sat_contraction.contract(d))
+                    time_taken_tensorsat = (time.time() - start_time_tensorsat) * 1000
+                    time_total_tensorsat += time_taken_tensorsat
+
+                    start_time_z3 = time.time()
                     solver.add(f)
                     if solver.check() == sat:
-                        print("sat")
-                        if not result:
-                            num_errors += 1
-                            print(line)
+                        # print("z3: sat")
+                        assert(result)
                         num_sat += 1
                     else:
-                        print("unsat")
+                        # print("z3: unsat")
                         if result:
-                            num_errors += 1
                             print(line)
+                        assert(not result)
                         num_unsat += 1
+
+                    # print(f"{num_sat} sat, {num_unsat} unsatisfiable.")
 
                     # print(f"Num errors: {num_errors}")
                     solver.pop()
-                    time_taken = (time.time() - start_time) * 1000
-                    time_total += time_taken
+                    time_taken_z3 = (time.time() - start_time_z3) * 1000
+                    time_total_z3 += time_taken_z3
                     # print("{} ms".format(time_taken))
                 else:
                     raise ParseError("Could not read formula.")
 
                 i += 1
 
-        # print(f"{num_sat} sat, {num_unsat} unsatisfiable.")
-        # print("Total time: {} seconds".format(time_total / 1000))
+        print(f"{num_sat} sat, {num_unsat} unsatisfiable.")
+        print("Z3 total time: {} seconds".format(time_total_z3 / 1000))
+        print("TensorSat total time: {} seconds".format(time_total_tensorsat / 1000))
 
 def main() -> None:
     benchmark_formulae()
